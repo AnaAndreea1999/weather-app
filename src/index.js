5d03622b12efb7783e9f4a13c2c591f4a7264276
AnaAndreea1999 <78683545+AnaAndreea1999@users.noreply.github.com>
2021-03-27 22:40:16 +0100

    Created html, css and JavaScript

---
+++ src/index.js
@@ -0,0 +1,89 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function location(event) {
+  event.preventDefault();
+  let cityValue = document.querySelector("#city-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = ` ${cityValue.value}`;
+}
+let newCity = document.querySelector("#search-form");
+newCity.addEventListener("submit", location);
+
+function weatherCondition(response) {
+  document.querySelector("#city-input").innerHTML = response.data.main.name;
+  document.querySelector("#temp-degrees").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "cb81b1e5f7f80e6e9cb0b2d42fb44664";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(weatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "cb81b1e5f7f80e6e9cb0b2d42fb44664";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp-degrees");
+  temperatureElement.innerHTML = 39;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp-degrees");
+  temperatureElement.innerHTML = 4;
+}
+let dateElement = document.querySelector("#ddate");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+let currentLocationButton = document.querySelector("#location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Basel");
+
